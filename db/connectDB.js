05acb65cf74158c333bf55dec6aa5bc8864f78f3
Shiv Chauhan <shivchauhan795@gmail.com>
2024-07-01 18:31:08 +0530

    connectDB corrcted

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URL, {
-            useUnifiedTopology: true,
-        });
+        const conn = await mongoose.connect(process.env.MONGODB_URL);
         console.log(`MongoDB Connected: {conn.connection.host}`);
     } catch (error) {
         console.error(error.message);
